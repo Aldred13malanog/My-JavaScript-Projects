77c04f436ff71f5bcb421de847e1f82fd06251de
Aldred <aldredmalanog@gmail.com>
2025-04-20 16:34:53 +0800

    create 2048 tile game & improved pomodoro timer and its css

--- Pomodoro-Timer-Project/scripts.js
+++ Pomodoro-Timer-Project/scripts.js
@@ -1,45 +1,85 @@
 const time = document.querySelector('.time');
 const start = document.querySelector('.start-btn');
-const stop = document.querySelector('.stop-btn');
+const stoP = document.querySelector('.stop-btn');
 const reset = document.querySelector('.reset-btn');
+const message = document.querySelector('.message');
 
 let minutes = 25;
 let seconds = 0;
+let breakTimeMinutes = 5;
+let breakTimeSeconds = 0;
 let interval;
+let isActive = true;
 
 function startTime() {
-	// clearInterval(interval);
-	stopTime();
-	interval = setInterval(updateTime, 1000);
+	pauseTime();
+	interval = isActive ? setInterval(updateTime, 1000) : setInterval(breakTimer, 1000);
 }
 
 function updateTime() {
 	if (minutes === 0 && seconds === 0) {
-		alert('Time is Up');
+		message.innerText = 'Time is Up';
 		clearInterval(interval);
+		isActive = false;
+
+		interval = setInterval(breakTimer, 1000);
+		time.innerText = '05:00';
+		message.innerText = "Break Time!"	;
+
+		minutes = 25;
+		seconds = 0;
 		return;
 	}
 
-	if (seconds <= 59 && seconds !== 0) {
+	if (seconds < 60 && seconds !== 0) {
 		seconds--;
 	} else {
 		minutes--;
 		seconds = 59;
 	}	
-	
+
 	time.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+	message.innerText = "Study Time!"	;
 }
 
-function stopTime() {
+function breakTimer() {
+	if (breakTimeMinutes === 0 && breakTimeSeconds === 0) {
+		message.innerText = 'Break Time is Up';
+		clearInterval(interval);
+		isActive = true;
+
+		interval = setInterval(updateTime, 1000);
+		time.innerText = '25:00';
+		message.innerText = "Study Time!";
+
+		breakTimeMinutes = 5;
+		breakTimeSeconds = 0;
+		return;
+	} 
+
+
+	if (breakTimeSeconds < 60 && breakTimeSeconds !== 0) {
+		breakTimeSeconds--;
+	} else {
+		breakTimeMinutes--;
+		breakTimeSeconds = 59;
+	}
+
+	time.innerText = `${breakTimeMinutes.toString().padStart(2, '0')}:${breakTimeSeconds.toString().padStart(2, '0')}`;
+}
+
+function pauseTime() {
 	clearInterval(interval);
 }
 
 function resetTime() {
-	time.innerText = '25:00';
+	time.innerText = isActive ? '25:00' : '05:00';
 	minutes = 25;
 	seconds = 0;
+	breakTimeMinutes = 5;
+	breakTimeSeconds = 0;
 }
 
 start.addEventListener('click', startTime);
-stop.addEventListener('click', stopTime);
+stoP.addEventListener('click', pauseTime);
 reset.addEventListener('click', resetTime);
